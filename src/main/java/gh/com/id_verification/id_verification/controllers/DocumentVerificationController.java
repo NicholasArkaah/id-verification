b2f8e1261271686acbaf675ae34263c493b2faea
NicholasArkaah <nicholasarkaah@gmail.com>
2024-10-06 14:04:39 +0000

    Updated NIA verification endpoint

--- src/main/java/gh/com/id_verification/id_verification/controllers/DocumentVerificationController.java
+++ src/main/java/gh/com/id_verification/id_verification/controllers/DocumentVerificationController.java
@@ -1,16 +1,13 @@
 package gh.com.id_verification.id_verification.controllers;
 
 
-import gh.com.id_verification.id_verification.dtos.requests.IdVerificationRequestDto;
 import gh.com.id_verification.id_verification.dtos.requests.IdVerificationServiceRequestDto;
-import gh.com.id_verification.id_verification.dtos.responses.IdVerificationResponseDto;
 import gh.com.id_verification.id_verification.services.DocumentVerificationServiceImpl;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -32,7 +29,7 @@ public class DocumentVerificationController {
             @ApiResponse(responseCode = "500", description = "There was an error while performing id verification. kindly contact Admin for support.")
     })
     @RequestMapping(value="/verify-file", produces = { "application/json" },consumes = {MediaType.MULTIPART_FORM_DATA_VALUE}, method = RequestMethod.POST)
-    public @ResponseBody ResponseEntity<IdVerificationResponseDto> queryIdVerificationWithFile( @RequestPart(value = "file") MultipartFile file,
+    public @ResponseBody ResponseEntity<String> queryIdVerificationWithFile( @RequestPart(value = "file") MultipartFile file,
                                                                                        @RequestPart String idNumber){
         return ResponseEntity.status(HttpStatus.OK).body(documentVerificationService.idVerificationRequest(file, idNumber));
     }
@@ -43,8 +40,8 @@ public class DocumentVerificationController {
             @ApiResponse(responseCode = "500", description = "There was an error while performing id verification. kindly contact Admin for support.")
     })
     @RequestMapping(value="/verify", produces = { "application/json" }, method = RequestMethod.POST)
-    public @ResponseBody ResponseEntity<IdVerificationResponseDto> queryIdVerification(@RequestBody @Valid IdVerificationServiceRequestDto requestDto){
-        return ResponseEntity.status(HttpStatus.OK).body(documentVerificationService.idVerificationRequestNofile(requestDto));
+    public @ResponseBody ResponseEntity<String> queryIdVerification(@RequestBody @Valid IdVerificationServiceRequestDto requestDto){
+        return ResponseEntity.status(HttpStatus.OK).body(documentVerificationService.idVerificationRequestNoFile(requestDto));
     }
 
 }
--- src/main/java/gh/com/id_verification/id_verification/services/DocumentVerificationServiceImpl.java
+++ src/main/java/gh/com/id_verification/id_verification/services/DocumentVerificationServiceImpl.java
@@ -1,25 +1,17 @@
 package gh.com.id_verification.id_verification.services;
 
-import gh.com.id_verification.id_verification.IdVerificationApplication;
 import gh.com.id_verification.id_verification.config.AppConfig;
-import gh.com.id_verification.id_verification.dtos.requests.IdVerificationRequestDto;
 import gh.com.id_verification.id_verification.dtos.requests.IdVerificationServiceRequestDto;
-import gh.com.id_verification.id_verification.dtos.responses.IdVerificationResponseDto;
 import gh.com.id_verification.id_verification.utils.Helper;
-import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.util.HashMap;
+import java.util.Collections;
 import java.util.Objects;
 
 @Service
@@ -31,10 +23,10 @@ public class DocumentVerificationServiceImpl {
 
     RestTemplate restTemplate = new RestTemplate();
 
-    ResponseEntity<IdVerificationResponseDto> response;
+    ResponseEntity<String> response;
 
     @Transactional
-    public IdVerificationResponseDto idVerificationRequest(MultipartFile file, String request) {
+    public String idVerificationRequest(MultipartFile file, String request) {
 
         //Create NIA Id Verification service request from user request
         IdVerificationServiceRequestDto serviceRequestDto = new IdVerificationServiceRequestDto();
@@ -46,62 +38,44 @@ public class DocumentVerificationServiceImpl {
 
 
 
-        MultiValueMap<String, String> headers = new LinkedMultiValueMap<>();
-        headers.add("Content-Type", "application/json");
-        headers.add("Accept", "application/json");
-
-        HashMap<String, String> requestPayload = new HashMap<>();
-        requestPayload.put("pinNumber", request);
-        requestPayload.put("center", appConfig.getCenter());
-        requestPayload.put("dataType",Helper.getExtensionByApacheCommonLib(Objects.requireNonNull(file.getOriginalFilename())).toUpperCase());
-        requestPayload.put("image",Helper.convertFileToBase64(file));
-        requestPayload.put("merchantKey",appConfig.getMerchant_key());
-
-        JSONObject requestObject = new JSONObject(requestPayload);
-        System.out.println("Request Payload: "+requestObject.toString());
-        //return restTemplate.postForObject(appConfig.getId_verification_url(), requestPayload, IdVerificationResponseDto.class);
-       try {
-             response = restTemplate.exchange(
+        IdVerificationServiceRequestDto requestDto = new IdVerificationServiceRequestDto();
+        requestDto.setMerchantKey(appConfig.getMerchant_key());
+        requestDto.setCenter(appConfig.getCenter());
+        requestDto.setDataType(Helper.getExtensionByApacheCommonLib(Objects.requireNonNull(file.getOriginalFilename())).toUpperCase());
+        requestDto.setImage(Helper.convertFileToBase64(file));
+        requestDto.setPinNumber(request);
+        try {
+            HttpHeaders httpHeaders = new HttpHeaders();
+            httpHeaders.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
+            HttpEntity<IdVerificationServiceRequestDto> entity = new HttpEntity<>(requestDto, httpHeaders);
+            response = restTemplate.exchange(
                     appConfig.getId_verification_url(),
-                    HttpMethod.POST, new HttpEntity<Object>(requestObject, headers),
-                    IdVerificationResponseDto.class);
+                    HttpMethod.POST,
+                    entity,
+                    String.class);
             return response.getBody();
         }catch (HttpClientErrorException ex){
-          return ex.getResponseBodyAs(IdVerificationResponseDto.class);
-       }
+            return ex.getResponseBodyAs(String.class);
+        }
 
     }
 
     @Transactional
-    public IdVerificationResponseDto idVerificationRequestNofile(IdVerificationServiceRequestDto requestDto) {
+    public String idVerificationRequestNoFile(IdVerificationServiceRequestDto requestDto) {
 
         //Create NIA Id Verification service request from user request
-
-
-
-
-        MultiValueMap<String, String> headers = new LinkedMultiValueMap<>();
-        headers.add("Content-Type", "application/json");
-        headers.add("Accept", "application/json");
-
-//        HashMap<String, String> requestPayload = new HashMap<>();
-//        requestPayload.put("pinNumber", request);
-//        requestPayload.put("center", appConfig.getCenter());
-//        requestPayload.put("dataType",Helper.getExtensionByApacheCommonLib(Objects.requireNonNull(file.getOriginalFilename())).toUpperCase());
-//        requestPayload.put("image",Helper.convertFileToBase64(file));
-//        requestPayload.put("merchantKey",appConfig.getMerchant_key());
-
-        JSONObject requestObject = new JSONObject(requestDto);
-        System.out.println("Request Payload: "+requestObject.toString());
-        //return restTemplate.postForObject(appConfig.getId_verification_url(), requestPayload, IdVerificationResponseDto.class);
         try {
+            HttpHeaders httpHeaders = new HttpHeaders();
+            httpHeaders.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
+            HttpEntity<IdVerificationServiceRequestDto> entity = new HttpEntity<>(requestDto, httpHeaders);
             response = restTemplate.exchange(
                     appConfig.getId_verification_url(),
-                    HttpMethod.POST, new HttpEntity<Object>(requestObject, headers),
-                    IdVerificationResponseDto.class);
+                    HttpMethod.POST,
+                    entity,
+                    String.class);
             return response.getBody();
         }catch (HttpClientErrorException ex){
-            return ex.getResponseBodyAs(IdVerificationResponseDto.class);
+            return ex.getResponseBodyAs(String.class);
         }
 
     }
